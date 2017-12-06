f9d40cd871703114494850445ca96c8253b625f4
root <mayankjha797@gmail.com>
2017-12-06 18:13:00 +0530

    model

--- EsDjango/admin.py
+++ EsDjango/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+    class Meta:
+        model = Person
+
+
+
+
+admin.site.register(Person, PersonAdmin)
--- EsDjango/models.py
+++ EsDjango/models.py
@@ -4,4 +4,16 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=250, null=False, blank=False)
     age = models.IntegerField(null=False,blank=False)
+    fname = models.CharField(max_length=250, null=False,blank=False)
+    city = models.CharField(max_length=150, null=True, blank=True)
+    state = models.CharField(max_length=150, null=True)
+    country = models.CharField(max_length=150, null=True, blank=True)
+    phone = models.IntegerField(null=False,blank=False)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
     
